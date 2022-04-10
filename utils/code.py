4ad6a328146ca4bda353f126158042b1c98ad102
arbanhossain <arbanhossain@gmail.com>
2022-04-10 19:24:09 +0600

    return c code if language does not exist

--- utils/code.py
+++ utils/code.py
@@ -1,7 +1,10 @@
 def send_code(lang, dirpath):
   if lang == None: lang = 'c'
-  with open(f'{dirpath}/{lang}.txt') as f:
-    return f.read()
+  try:
+    with open(f'{dirpath}/{lang}.txt') as f:
+      return f.read()
+  except:
+    return send_code('c', dirpath)
 
 if __name__ == "__main__":
   pass
